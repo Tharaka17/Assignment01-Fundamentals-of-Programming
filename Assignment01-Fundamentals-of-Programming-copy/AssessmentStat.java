b4d11d15fbc4083f0cba83174f12b796433f45dc
Tharaka17 <tharakaarrow@gmail.com>
2023-09-11 14:37:49 +1000

    Get the marks for each student

--- Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
+++ Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
@@ -10,5 +10,11 @@ public class AssessmentStat {
         
         // Create an array to store 30 students marks 
         int[] studentMarks = new int[30];
+        
+         //Get the marks for each student
+        for (int i = 0; i < studentMarks.length; i++) {
+            System.out.print("Please input the marks for Student " + (i + 1) + ": ");
+            int mark = scanner.nextInt();
+        }
     }
 }
