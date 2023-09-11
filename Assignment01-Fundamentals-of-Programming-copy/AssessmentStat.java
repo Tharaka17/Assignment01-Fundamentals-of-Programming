18de19e810c5c1cec337e2cfbde0f685cd92c3e4
Tharaka17 <tharakaarrow@gmail.com>
2023-09-11 14:13:13 +1000

    Get the assessments name from the user

---
+++ Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AssessmentStat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the assessment name from the user
+        System.out.print("Enter the Assessment name: ");
+        String assessnmentName = scanner.nextLine();
+    }
+}
