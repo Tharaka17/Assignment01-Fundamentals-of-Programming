02f94ebaaee468eff956dea7052dd07310158df4
Tharaka17 <tharakaarrow@gmail.com>
2023-09-11 14:55:13 +1000

    Check if all students have been assigned marks

--- Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
+++ Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
@@ -7,21 +7,33 @@ public class AssessmentStat {
         // Get the assessment name from the user
         System.out.print("Enter the  name: ");
         String assessnmentName = scanner.nextLine();
-        
+
         // Create an array to store 30 students marks 
         int[] studentMarks = new int[30];
-        
-         //Get the marks for each student
+
+        //Get the marks for each student
         for (int i = 0; i < studentMarks.length; i++) {
             System.out.print("Please input the marks for Student " + (i + 1) + ": ");
             int mark = scanner.nextInt();
-            
-             // For check Validation of the mark
+
+            // For check Validation of the mark
             if (mark < 0 || mark > 30) {
                 System.out.println("Error: Invalid mark. Please enter a mark between 0 and 30.");
                 i--; 
             } else {
                 studentMarks[i] = mark;
+            }
+
+            // Check if all students have been assigned marks
+            if (i == studentMarks.length - 1) {
+                System.out.println("Maximum number of students get.");
+            } else {
+                System.out.print("Do you wish to enter marks for another student? (yes/no): ");
+                String response = scanner.next();
+
+                if (!response.equalsIgnoreCase("yes")) {
+                    break; 
+                }
             }
         }
     }
