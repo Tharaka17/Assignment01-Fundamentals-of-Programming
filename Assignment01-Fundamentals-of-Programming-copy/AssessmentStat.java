c1abf449e7c08bf3456ce686e4fd4aa1c7302258
Tharaka17 <tharakaarrow@gmail.com>
2023-09-11 14:32:31 +1000

    Create an array to store 30 students marks

--- Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
+++ Assignment01-Fundamentals-of-Programming-copy/AssessmentStat.java
@@ -5,7 +5,10 @@ public class AssessmentStat {
         Scanner scanner = new Scanner(System.in);
 
         // Get the assessment name from the user
-        System.out.print("Enter the Assessment name: ");
+        System.out.print("Enter the  name: ");
         String assessnmentName = scanner.nextLine();
+        
+        // Create an array to store 30 students marks 
+        int[] studentMarks = new int[30];
     }
 }
